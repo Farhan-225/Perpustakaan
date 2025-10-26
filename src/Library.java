c1204ea357e73a75258b300e9317f18322f9670e
Farhan-225 <saylmly@webmail.umm.ac.id>
2025-10-27 05:54:54 +0700

    Add files via upload

---
+++ src/Library.java
@@ -0,0 +1,60 @@
+/**
+ * ==========================================================
+ * Kelas Library
+ * ==========================================================
+ * Deskripsi:
+ * Kelas ini merepresentasikan sebuah perpustakaan yang memiliki
+ * lokasi dan menyimpan satu objek Book. Menyediakan method untuk
+ * menampilkan informasi perpustakaan dan buku yang tersedia.
+ *
+ * @author
+ * Mochammad Farhan Hikmadi
+ * @version
+ * 1.0
+ * @since
+ * 2025-10-27
+ */
+public class Library {
+    private Book book;
+    private String location;
+
+    /**
+     * Konstruktor untuk membuat objek Library baru.
+     *
+     * @param book     Objek buku yang ada di perpustakaan
+     * @param location Lokasi perpustakaan
+     */
+    public Library(Book book, String location) {
+        this.book = book;
+        this.location = location;
+    }
+
+    /** @return Objek Book */
+    public Book getBook() {
+        return book;
+    }
+
+    /** @param book Objek Book yang baru */
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    /** @return Lokasi perpustakaan */
+    public String getLocation() {
+        return location;
+    }
+
+    /** @param location Lokasi perpustakaan yang baru */
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    /**
+     * Menampilkan informasi lokasi perpustakaan
+     * dan data buku yang ada di dalamnya.
+     */
+    public void showLibraryInfo() {
+        System.out.println("Library Location: " + getLocation());
+        book.displayInfo();
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,40 @@
+/**
+ * ==========================================================
+ * Kelas Main
+ * ==========================================================
+ * Deskripsi:
+ * Kelas ini merupakan entry point program.
+ * Program ini membuat objek Book dan Library, menampilkan informasi,
+ * serta menyesuaikan stok buku untuk memperlihatkan perubahan data.
+ *
+ * @author
+ * Mochammad Farhan Hikmadi
+ * @version
+ * 1.0
+ * @since
+ * 2025-10-27
+ */
+public class Main {
+    /**
+     * Method utama (entry point) program.
+     * Membuat objek Book dan Library, lalu menampilkan serta memperbarui datanya.
+     *
+     * @param args Argumen baris perintah (tidak digunakan)
+     */
+    public static void main(String[] args) {
+        // Membuat objek Book
+        Book book1 = new Book("Harry Potter", "J.K. Rowling", 10.2, 2);
+
+        // Membuat objek Library
+        Library lib = new Library(book1, "Perpustakaan Kota");
+
+        // Display initial information
+        lib.showLibraryInfo();
+
+        // Add more stock
+        book1.adjustStock(5);
+
+        // Display updated information
+        lib.showLibraryInfo();
+    }
+}
