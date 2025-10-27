168b9fc2781ca7532db1b4206d1ad26482a50a72
Farhan-225 <saylmly@webmail.umm.ac.id>
2025-10-27 08:00:24 +0700

    h

--- src/Library.java
+++ src/Library.java
@@ -58,3 +58,4 @@ public class Library {
         book.displayInfo();
     }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -23,6 +23,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // Membuat objek Book
+        int i = 1+1;
         Book book1 = new Book("Harry Potter", "J.K. Rowling", 10.2, 2);
 
         // Membuat objek Library
